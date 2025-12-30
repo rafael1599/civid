9ad638ff9640b3ff33f7fe28b3137b651dea46d2
rafael1599 <rafaelukf@gmail.com>
2025-12-29 23:10:52 -0500

    feat: add initial database configuration and create migration for the entities table.

--- database/migrations/2025_12_26_050230_create_entities_table.php
+++ database/migrations/2025_12_26_050230_create_entities_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,13 +13,17 @@ return new class extends Migration
         Schema::create('entities', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignUuid('parent_entity_id')->nullable()->constrained('entities')->nullOnDelete();
+            $table->foreignUuid('parent_entity_id')->nullable();
             $table->string('name');
             $table->string('category'); // 'ASSET', 'HEALTH', 'EDUCATION', 'FINANCE', 'PROJECT'
             $table->string('status')->default('ACTIVE');
             $table->jsonb('metadata')->nullable();
             $table->softDeletes();
             $table->timestamps();
+        });
+
+        Schema::table('entities', function (Blueprint $table) {
+            $table->foreign('parent_entity_id')->references('id')->on('entities')->nullOnDelete();
         });
     }
 
